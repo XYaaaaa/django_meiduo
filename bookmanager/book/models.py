99f73f09ac00576d39eb18b687d4d023141619c0
xiaoyao <xiaoyao96324@gmail.com>
2021-01-24 15:23:16 -0800

    define models

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+# ORM model
 # Create your models here.
+
+class BookInfo(models.Model): #book information
+    #id inherit
+    name = models.CharField(max_length=10)
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    #key constraint
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
